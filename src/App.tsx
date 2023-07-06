36d6f06b028486b3517e64e863e780429fa9bcf2
Sam Marxz <sammarxzz@gmail.com>
2023-07-06 11:03:17 -0300

    add react-router-dom and configure routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import { History, Home } from './pages'
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/history' element={<History />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
