8f1b9aca7e8d6738569e4129e2f26c36e674095e
Sam Marxz <sammarxzz@gmail.com>
2023-07-06 12:08:24 -0300

    create static unstyled home

--- src/layout/DefaultLayout/DefaultLayout.tsx
+++ src/layout/DefaultLayout/DefaultLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
 
-import { Header } from '../components'
+import { Header } from '@/components'
 
 export function DefaultLayout() {
   return (
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,45 @@
 export function Home() {
-  return <div>Home</div>
+  return (
+    <div>
+      <form action="">
+        <div>
+          <label htmlFor="task">I&apos;m going to work on</label>
+          <input
+            type="text"
+            id="task"
+            list="task-suggestions"
+            placeholder="Name your project"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Project 1" />
+            <option value="Project 2" />
+            <option value="Project 3" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">for</label>
+          <input
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutes.</span>
+        </div>
+
+        <div>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </div>
+
+        <button type="submit">Start</button>
+      </form>
+    </div>
+  )
 }
