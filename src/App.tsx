5c6fa38d28f68eb81f22a55f33192558f9e2f184
Sam Marxz <sammarxzz@gmail.com>
2023-07-06 11:48:01 -0300

    add default layout to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+import { DefaultLayout } from './layout/DefaultLayout'
 import { History, Home } from './pages'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/history' element={<History />} />
+        <Route path="/" element={<DefaultLayout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/history' element={<History />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
