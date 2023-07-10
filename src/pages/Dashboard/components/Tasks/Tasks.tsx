7d87aeb0b503b576b74855ed18979374debc6aa1
Sam Marxz <sammarxzz@gmail.com>
2023-07-10 15:00:36 -0300

    (feature) CRUD tasks and reorder tasks with framer-motion

--- src/pages/Dashboard/components/Tasks/Tasks.tsx
+++ src/pages/Dashboard/components/Tasks/Tasks.tsx
@@ -1,27 +1,64 @@
+import { Heading, Flex, IconButton, Icon, VStack } from '@chakra-ui/react'
+import { FiColumns, FiList } from 'react-icons/fi'
+import { AnimatePresence, Reorder } from 'framer-motion'
+
 import { useTask } from '@/hooks'
+
+import { reorderTasks } from '@/store/task/reducers/actions'
+import { type TaskData } from '@/store/task/reducers'
 
 import { Task } from '../Task'
 
 export function Tasks() {
   const {
-    state: { tasks }
+    state: { tasks },
+    dispatch
   } = useTask()
 
+  function handleReorder(tasks: TaskData[]) {
+    dispatch(reorderTasks(tasks))
+  }
+
   return (
-    <table border={1} width={400}>
-      <thead>
-        <tr>
-          <th></th>
-          <th>Task</th>
-          <th>Time</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {tasks.map((task) => (
-          <Task key={task.id} task={task} />
-        ))}
-      </tbody>
-    </table>
+    <Flex w="full" direction="column" gap={6}>
+      <Flex align="center" justifyContent="space-between">
+        <Heading as="h2" size="lg" fontWeight="bold" color="brand.500">
+          Tasks
+        </Heading>
+        <Flex align="center">
+          <IconButton
+            icon={<Icon as={FiColumns} />}
+            aria-label="View tasks in column mode"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            variant="unstyled"
+            color="gray.300"
+            fontSize="xl"
+          />
+          <IconButton
+            icon={<Icon as={FiList} />}
+            aria-label="View tasks in List mode"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            variant="unstyled"
+            fontSize="xl"
+          />
+        </Flex>
+      </Flex>
+      <Reorder.Group axis="y" onReorder={handleReorder} values={tasks}>
+        <VStack
+          as={AnimatePresence}
+          initial={false}
+          alignItems="flex-start"
+          w="full"
+        >
+          {tasks.map((task) => (
+            <Task key={task.id} task={task} />
+          ))}
+        </VStack>
+      </Reorder.Group>
+    </Flex>
   )
 }
