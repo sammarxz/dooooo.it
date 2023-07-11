f1ce4a94c174698baacb28d907008bad680ed1ae
Sam Marxz <sammarxzz@gmail.com>
2023-07-11 18:23:20 -0300

    (feature) use local storage

--- src/store/app.provider.tsx
+++ src/store/app.provider.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode, useReducer } from 'react'
+import { type ReactNode, useReducer, useEffect } from 'react'
 
 import { AppContext, initialState } from './app.context'
 
@@ -8,8 +8,28 @@ interface AppContextProviderProps {
   children: ReactNode
 }
 
+const LOCAL_STORAGE_KEY = '@timerit:state-1.0.0'
+
 export function AppContextProvider({ children }: AppContextProviderProps) {
-  const [state, dispatch] = useReducer(appReducer, initialState as never)
+  const [state, dispatch] = useReducer(
+    appReducer,
+    initialState as never,
+    (initialState) => {
+      const storedStateAsJSON = localStorage.getItem(LOCAL_STORAGE_KEY)
+
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON)
+      }
+
+      return initialState
+    }
+  )
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(state)
+
+    localStorage.setItem(LOCAL_STORAGE_KEY, stateJSON)
+  }, [state])
 
   return (
     <AppContext.Provider value={{ state, dispatch }}>
