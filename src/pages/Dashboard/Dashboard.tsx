f3241303130d47145064477d62de0c97d82f5d4a
Sam Marxz <sammarxzz@gmail.com>
2023-07-11 18:17:56 -0300

    add framer motion animations on sections

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
-import { VStack } from '@chakra-ui/react'
+import { Box, VStack } from '@chakra-ui/react'
+import { AnimatePresence, motion } from 'framer-motion'
 
 import { AddSection, Header, Section } from './components'
 import { useAppContext } from '@/hooks'
@@ -9,9 +10,21 @@ export function Dashboard() {
   return (
     <VStack spacing={16} align="flex-start">
       <Header title="Project 1" />
-      {state.sections.map((section) => (
-        <Section key={section.id} section={section} />
-      ))}
+      <Box as={AnimatePresence} w="full">
+        {state.sections.map((section) => (
+          <Box
+            as={motion.div}
+            key={section.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            layout
+            w="full"
+          >
+            <Section section={section} />
+          </Box>
+        ))}
+      </Box>
       <AddSection />
     </VStack>
   )
