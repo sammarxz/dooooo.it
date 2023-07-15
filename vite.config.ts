4e8ecc1c58a0af4f99582c1248987c9584518260
Sam Marxz <sammarxzz@gmail.com>
2023-07-15 11:07:59 -0300

    (chore) configure test env

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }]
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom'
   }
 })
