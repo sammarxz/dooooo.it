e90b9b3424ef7c9dfc6871d1b5277a10e76db9a3
Sam Marxz <sammarxzz@gmail.com>
2023-07-15 11:08:58 -0300

    (chore) leave home empty for now

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,51 +1,7 @@
-import { CustomModal } from '@/components'
-
 export function Home() {
-  const handleModal1Open = () => {
-    console.log('Modal 1 aberto')
-  }
-
-  const handleModal1Close = () => {
-    console.log('Modal 1 fechado')
-  }
-
-  const handleModal2Open = () => {
-    console.log('Modal 2 aberto')
-  }
-
-  const handleModal2Close = () => {
-    console.log('Modal 2 fechado')
-  }
-
   return (
     <div>
-      <CustomModal
-        showModalButtonText="Open Modal 1"
-        modalId="modal1"
-        onOpen={handleModal1Open}
-        onClose={handleModal1Close}
-      >
-        <CustomModal.Header>Modal 1 Header</CustomModal.Header>
-        <p>Modal 1 Content</p>
-        <CustomModal.Footer>
-          <button>Cancel</button>
-          <button>Delete</button>
-        </CustomModal.Footer>
-      </CustomModal>
-
-      <CustomModal
-        showModalButtonText="Open Modal 2"
-        modalId="modal2"
-        onOpen={handleModal2Open}
-        onClose={handleModal2Close}
-      >
-        <CustomModal.Header>Modal 2 Header</CustomModal.Header>
-        <p>Modal 2 Content</p>
-        <CustomModal.Footer>
-          <button>Cancel</button>
-          <button>Delete</button>
-        </CustomModal.Footer>
-      </CustomModal>
+      <h1>Hello World</h1>
     </div>
   )
 }
